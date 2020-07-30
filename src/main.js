c2875efa2887a8bb14afcdb795882ac76b71b1a8
Jozy Kinnaman <jozypants@gmail.com>
2020-07-30 12:31:18 -0700

    retrieve appropriate API call response.

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
   const data = async () => new Promise(resolve => {
     setTimeout(() => resolve(window.apiResponse), 1000)
   });
-
+console.log(window.apiResponse)
 
 
 
@@ -18,11 +18,22 @@ $(document).ready(function() {
   }
  
 
-    async () => {
+    (async () => {
     const $sort = document.getElementById('sort')
     $sort.addEventListener('change', sortReleases)
-
     const response = await data()
     console.log(`${response.releases.length} Releases`)
+    getElements(response);
+      // $("#card").html(`<h3>${response.releases.length} </h3>`);
+  })();
+  function getElements(response) {
+    if (response) {
+      $("#cards").html(`<h3>${response.releases[0].artist}</h3>`);
+    } else if (response === false) {
+      $("#cards").html(`<h3> Release cannot be found. </h3>`
+      );
+    } else {
+      $('#cards').html(`<h3> ${response}</h3>`);
+    }
   }
 });
